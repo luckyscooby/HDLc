6b3076aa2d572703d96656add2d8498f9e422753
Michael Leocádio <leocadiomichael@outlook.com>
2024-01-01 16:06:11 -0300

    Improved icons

--- HDLc/resource.h
+++ HDLc/resource.h
@@ -2,14 +2,14 @@
 // Arquivo de inclusão gerado pelo Microsoft Visual C++.
 // Usado por HDLc.rc
 //
-#define IDI_ICON1                       104
-#define IDI_ICON2                       105
+#define IDI_ICON1                       101
+#define IDI_ICON2                       103
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
