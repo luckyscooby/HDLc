f9394cd4ed4908fad9996a8c64c969e418c77fba
Michael Leocádio <leocadiomichael@outlook.com>
2024-01-01 16:15:39 -0300

    Fix double instantiation on install script

--- HDLc/Install.cmd
+++ HDLc/Install.cmd
@@ -5,5 +5,4 @@ echo Installing...
 copy %~dp0HDLc.exe /B %windir% /Y
 reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v HDLc /d %windir%\HDLc.exe /f
 start %windir%\HDLc.exe
-start %~dp0%HDLc.exe
 pause
